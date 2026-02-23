bd2e70c7d623a42a88cfe98c16612d8eb27e7bbd
kimzaaa <112327567+kimzaaa@users.noreply.github.com>
2026-02-23 22:26:30 +0700

    transitional page swapping

--- Oscillate/Views/Journey/JourneyMain.swift
+++ Oscillate/Views/Journey/JourneyMain.swift
@@ -1,9 +1,34 @@
 import SwiftUI
 
-struct JourneyMain: View{
+struct JourneyMain: View {
+    @State private var showLevel1 = false
+
     var body: some View {
-        VStack{
-            /*@START_MENU_TOKEN@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
+        ZStack {
+            if showLevel1 {
+                Level1Main()
+                    .transition(.opacity)
+            } else {
+                VStack {
+                    Text("Journey Mode")
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    Button(action: {
+                        withAnimation(.easeInOut(duration: 0.5)) {
+                            showLevel1 = true
+                        }
+                    }) {
+                        Text("Start Level 1")
+                            .font(.headline)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+                .transition(.opacity)
+            }
         }
     }
 }
